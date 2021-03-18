7b0a1d6ba51c0e63db14f1bace7e8bca68655354
johnmathews <mthwsjc@gmail.com>
2021-03-18 22:59:08 +0100

    attempt 3

--- theme/js/themeChooser.js
+++ theme/js/themeChooser.js
@@ -1,30 +1,54 @@
 $( document ).ready(function() {
 
-    // swap out pygment CSS files based on dark/light theme
-    function changePygmentsCSS(existing, updated) {
-        var PygmentCSS = document.getElementById("PygmentCSS");
-        PygmentCSS.setAttribute("href", updated);
-    }
+    if (document.getElementById('PygmentCSS') !=null) {
+        // swap out pygment CSS files based on dark/light theme
+        function changePygmentsCSS(existing, updated) {
+            var PygmentCSS = document.getElementById("PygmentCSS");
+            PygmentCSS.setAttribute("href", updated);
+        }
+    } else {
+        function changePygmentsCSS(a, b) {}
+    };
+
+
+    if (document.getElementById('hamburger') !=null) {
+        document.getElementById("hamburger").onclick = function toggleMenu() {
+            const navToggle = document.getElementsByClassName("toggle");
+            for (let i = 0; i < navToggle.length; i++) {
+                console.log(navToggle.item(i));
+                navToggle.item(i).classList.toggle("hidden");
+            }
+        };
+    };
 
     // set dark theme based on local storage settings
     if (localStorage.getItem('theme') === "dark") {
         document.getElementById("baz").classList.add("dark");
-        document.getElementsByClassName("day")[0].classList.remove("hidden");
-        document.getElementsByClassName("night")[0].classList.add("hidden");
-        document.getElementsByClassName("day")[1].classList.remove("hidden");
-        document.getElementsByClassName("night")[1].classList.add("hidden");
-        document.getElementsByClassName("day")[2].classList.remove("hidden");
-        document.getElementsByClassName("night")[2].classList.add("hidden");
+
+        const dayElement = document.getElementsByClassName("day");
+        for (let i = 0; i < dayElement.length; i++) {
+          dayElement.item(i).classList.remove("hidden");
+        }
+        const nightElement = document.getElementsByClassName("night");
+        for (let i = 0; i < nightElement.length; i++) {
+          dayElement.item(i).classList.add("hidden");
+        }
+
         changePygmentsCSS(light, dark);
+
     } else {
         document.getElementById("baz").classList.remove("dark");
         window.localStorage.setItem('theme', 'light');
-        document.getElementsByClassName("day")[0].classList.add("hidden");
-        document.getElementsByClassName("night")[0].classList.remove("hidden");
-        document.getElementsByClassName("day")[1].classList.add("hidden");
-        document.getElementsByClassName("night")[1].classList.remove("hidden");
-        document.getElementsByClassName("day")[2].classList.add("hidden");
-        document.getElementsByClassName("night")[2].classList.remove("hidden");
+
+        const dayElement = document.getElementsByClassName("day");
+        for (let i = 0; i < dayElement.length; i++) {
+          dayElement.item(i).classList.add("hidden");
+        }
+        const nightElement = document.getElementsByClassName("night");
+        for (let i = 0; i < nightElement.length; i++) {
+          dayElement.item(i).classList.remove("hidden");
+        }
+
         changePygmentsCSS(dark, light);
     }
 
@@ -36,28 +60,34 @@ $( document ).ready(function() {
             $( "#baz" ).removeClass( "dark" );
             changePygmentsCSS(dark, light);
 
-            document.getElementsByClassName("day")[0].classList.add("hidden");
-            document.getElementsByClassName("night")[0].classList.remove("hidden");
-            document.getElementsByClassName("day")[1].classList.add("hidden");
-            document.getElementsByClassName("night")[1].classList.remove("hidden");
-            document.getElementsByClassName("day")[2].classList.add("hidden");
-            document.getElementsByClassName("night")[2].classList.remove("hidden");
+            const dayElement = document.getElementsByClassName("day");
+            for (let i = 0; i < dayElement.length; i++) {
+              dayElement.item(i).classList.add("hidden");
+            }
+            const nightElement = document.getElementsByClassName("night");
+            for (let i = 0; i < nightElement.length; i++) {
+              dayElement.item(i).classList.remove("hidden");
+            }
+
         } else {
             window.localStorage.setItem('theme', 'dark');
             $( "#baz" ).addClass( "dark" );
             changePygmentsCSS(light, dark);
 
-            document.getElementsByClassName("day")[0].classList.remove("hidden");
-            document.getElementsByClassName("night")[0].classList.add("hidden");
-            document.getElementsByClassName("day")[1].classList.remove("hidden");
-            document.getElementsByClassName("night")[1].classList.add("hidden");
-            document.getElementsByClassName("day")[2].classList.remove("hidden");
-            document.getElementsByClassName("night")[2].classList.add("hidden");
+            const dayElement = document.getElementsByClassName("day");
+            for (let i = 0; i < dayElement.length; i++) {
+              dayElement.item(i).classList.remove("hidden");
+            }
+            const nightElement = document.getElementsByClassName("night");
+            for (let i = 0; i < nightElement.length; i++) {
+              dayElement.item(i).classList.add("hidden");
+            }
         }
 
         // TODO: close the hamburge menu after dark mode is clicked
     }
 
+    $( "#theme-toggle" ).click(modeSwitcher);
     $( "#modeSwitcher1" ).click(modeSwitcher);
     $( "#modeSwitcher2" ).click(modeSwitcher);
     $( "#modeSwitcher3" ).click(modeSwitcher);
