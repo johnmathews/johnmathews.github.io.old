db955ec5fdb3ddba64ee0bb04f94d07bd4b032a8
johnmathews <mthwsjc@gmail.com>
2021-03-28 16:59:41 +0200

    fix navbar sticky show/hide

--- theme/js/navbar.js
+++ theme/js/navbar.js
@@ -1,13 +1,15 @@
-// navbar hide on scroll
-var prevScrollPos = window.pageYOffset
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset
-  if (currentScrollPos > 50) {
-    if (prevScrollPos > currentScrollPos) {
-      document.getElementById('navbar').style.top = '0'
-    } else {
-      document.getElementById('navbar').style.top = '-100px'
+$( document ).ready(function() {
+  // navbar hide on scroll
+  var prevScrollPos = window.pageYOffset
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset
+    if (currentScrollPos > 50) {
+      if (prevScrollPos > currentScrollPos) {
+        document.getElementById('navbar').style.top = '0px'
+      } else {
+        document.getElementById('navbar').style.top = '-100px'
+      }
+      prevScrollPos = currentScrollPos;
     }
-    prevScrollPos = currentScrollPos
   }
-}
+})
