ca81138369c35a92f66bdb396783c90778f28d66
johnmathews <mthwsjc@gmail.com>
2021-04-09 11:32:52 +0200

    setup eslint. did stuff break?

--- theme/js/keyboard-shortcuts.js
+++ theme/js/keyboard-shortcuts.js
@@ -1,26 +1,27 @@
 import './mouseTrap.js'
 
-function goToSelected(){
-  $(".selected")[0].click()
+/* global Mousetrap siteURL chooseBoth chooseTechnical chooseNonTechnical */
+
+function goToSelected () {
+  $('.selected')[0].click()
 }
 
-function focusSelectedListItem(n){
-  var focusClasses = "selected underline"
+function focusSelectedListItem (n) {
+  var focusClasses = 'selected underline'
 
   var listLength = $('.listItem').length
   for (var i = 0; i < listLength; i++) {
-    if ( i == n ) {
-      $("#primaryList .listItem").eq(i).find('a').addClass(focusClasses);
+    if (i == n) {
+      $('#primaryList .listItem').eq(i).find('a').addClass(focusClasses)
     } else {
-      $("#primaryList .listItem").eq(i).find('a').removeClass(focusClasses);
+      $('#primaryList .listItem').eq(i).find('a').removeClass(focusClasses)
     }
   }
 }
 
-
-function incrementFocus(direction){
+function incrementFocus (direction) {
   // if there isn't a list, dont do anything
-  if (!$(".listItem")[0]) {
+  if (!$('.listItem')[0]) {
     return
   } else {
     var listLength = $('.listItem').length
@@ -28,44 +29,44 @@ function incrementFocus(direction){
 
   // check that search interface isnt showing
   if ($('#fuseModal').hasClass('hidden')) {
-    if (direction === 'f'){
+    if (direction === 'f') {
       if (window.selectedListItem == listLength) {
-        window.selectedListItem = 0 ;
+        window.selectedListItem = 0
       } else {
-        window.selectedListItem++;
-      };
-    } else if (direction === 'b'){
+        window.selectedListItem++
+      }
+    } else if (direction === 'b') {
       if (window.selectedListItem == 0) {
         window.selectedListItem = listLength
       } else {
-        window.selectedListItem-- ;
-      };
+        window.selectedListItem--
+      }
     }
     focusSelectedListItem(window.selectedListItem)
   }
-};
+}
 
 $(document).ready(function () {
   window.selectedListItem = -1
 
-  Mousetrap.bind('ctrl+j', function(){ incrementFocus('f') })
-  Mousetrap.bind('ctrl+k', function(){incrementFocus('b')})
+  Mousetrap.bind('ctrl+j', function () { incrementFocus('f') })
+  Mousetrap.bind('ctrl+k', function () { incrementFocus('b') })
   Mousetrap.bind('return', goToSelected)
   Mousetrap.bind('j', function () {
-    window.scrollBy({top: 200, left: 0, behavior: 'smooth'})
+    window.scrollBy({ top: 200, left: 0, behavior: 'smooth' })
   })
   Mousetrap.bind('k', function () {
-    window.scrollBy({top: -200, left: 0, behavior: 'smooth'})
+    window.scrollBy({ top: -200, left: 0, behavior: 'smooth' })
   })
   Mousetrap.bind('g i', function () {
     window.location.href = `${siteURL}/blog.html`
   })
   Mousetrap.bind('G', function () {
-    $("html, body").animate({ scrollTop: $(document).height()-$(window).height()}, 500)
+    $('html, body').animate({ scrollTop: $(document).height() - $(window).height() }, 500)
   })
   Mousetrap.bind('g g', function () {
     console.log('smoke')
-    $("html, body").animate({ scrollTop: 0}, 500)
+    $('html, body').animate({ scrollTop: 0 }, 500)
   })
   Mousetrap.bind('g b', function () {
     window.history.back()
@@ -74,17 +75,17 @@ $(document).ready(function () {
     window.history.forward()
   })
   Mousetrap.bind('g n', function () {
-    if ($( "#nextArticleInCategory").length ) {
+    if ($('#nextArticleInCategory').length) {
       $('#nextArticleInCategory')[0].click()
     }
   })
   Mousetrap.bind('g p', function () {
-    if ($( "#previousArticleInCategory").length ) {
+    if ($('#previousArticleInCategory').length) {
       $('#previousArticleInCategory')[0].click()
     }
   })
   Mousetrap.bind('g o', function () {
-    if ($( "#otherArticlesInCategory").length ) {
+    if ($('#otherArticlesInCategory').length) {
       $('#otherArticlesInCategory')[0].click()
     }
   })
@@ -125,12 +126,12 @@ $(document).ready(function () {
     $('#keyboardShortcuts').toggleClass('hidden')
   })
   Mousetrap.bind('esc', function () {
-    document.getElementById("searchBox").blur();
-    if ($("#fuseModal").hasClass('hidden') === false) {
-      document.getElementById("searchBox").blur();
-      $('#fuseModal').addClass('hidden');
-    } else if ($("#keyboardShortcuts").hasClass('hidden') === false) {
-      $('#keyboardShortcuts').addClass('hidden');
+    document.getElementById('searchBox').blur()
+    if ($('#fuseModal').hasClass('hidden') === false) {
+      document.getElementById('searchBox').blur()
+      $('#fuseModal').addClass('hidden')
+    } else if ($('#keyboardShortcuts').hasClass('hidden') === false) {
+      $('#keyboardShortcuts').addClass('hidden')
     }
   })
 })
