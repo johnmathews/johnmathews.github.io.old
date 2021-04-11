abc3c969e9075e7ae6bbd2f12f6c4580de434617
johnmathews <mthwsjc@gmail.com>
2021-04-11 08:39:20 +0200

    blogging

--- theme/js/fuseSearch.js
+++ theme/js/fuseSearch.js
@@ -1,15 +1,17 @@
+/* global hotkeys indexURL */
+
 import Fuse from './fuse.esm.js'
 import _ from './underscore.js'
 import './hotkeys.min.js'
 
-async function get() {
-  let obj = await (await fetch(indexURL)).json();
-  return obj;
+async function get () {
+  const obj = await (await fetch(indexURL)).json()
+  return obj
 }
 
-var searchIndex;
+var searchIndex
 
-window.resultsLength = 0;
+window.resultsLength = 0
 window.focussedResult = -1;
 
 (async () => {
@@ -27,140 +29,136 @@ window.focussedResult = -1;
     // location: 0, // where in the text is the match expected
     // threshold: 1, // match anything within `distance` of `location`
     keys: [
-      {name: "title", weight: 0.9},
-      {name: "tags", weight: 0.7},
-      {name: "body", weight: 0.7},
+      { name: 'title', weight: 1.0 },
+      { name: 'tags', weight: 0.7 },
+      { name: 'body', weight: 0.4 }
       // {name: "category", weight: 0.3},
       // {name: "url", weight: 0.3},
     ]
-  };
-
-  window.fuse = new Fuse(searchIndex, options);
-})()
-
-$( document ).ready(function() {
-
-  // show search modal if link is clicked
-  function showFuseSearch() {
-    window.focussedResult = -1;
-    $('#fuseModal').toggleClass('hidden');
-    $('#searchBox').focus();
   }
 
+  window.fuse = new Fuse(searchIndex, options)
+})()
 
-  $( "#fuseSearch" ).click(showFuseSearch);
-  $( "#searchButton" ).click(showFuseSearch);
-  $( "#searchButton2" ).click(showFuseSearch);
-  $( "#searchButton3" ).click(showFuseSearch);
+$(document).ready(function () {
+  // show search modal if link is clicked
+  function showFuseSearch () {
+    window.focussedResult = -1
+    $('#fuseModal').toggleClass('hidden')
+    $('#searchBox').focus()
+  }
 
-  $( "#searchModalBackground" ).click(function() {
-    $('#fuseModal').toggleClass('hidden');
-  });
+  $('#fuseSearch').click(showFuseSearch)
+  $('#searchButton').click(showFuseSearch)
+  $('#searchButton2').click(showFuseSearch)
+  $('#searchButton3').click(showFuseSearch)
 
-  hotkeys('command+k,/,esc,return', function (event, handler){
-    event.preventDefault();
+  $('#searchModalBackground').click(function () {
+    $('#fuseModal').toggleClass('hidden')
+  })
+
+  hotkeys('command+k,/,esc,return', function (event, handler) {
+    event.preventDefault()
     switch (handler.key) {
       case 'command+k':
-        window.focussedResult = -1;
-        $('#fuseModal').toggleClass('hidden');
-        $('#searchBox').focus();
-        break;
+        window.focussedResult = -1
+        $('#fuseModal').toggleClass('hidden')
+        $('#searchBox').focus()
+        break
       case '/':
-        window.focussedResult = -1;
-        $('#fuseModal').toggleClass('hidden');
-        $('#searchBox').focus();
-        break;
+        window.focussedResult = -1
+        $('#fuseModal').toggleClass('hidden')
+        $('#searchBox').focus()
+        break
     }
-  });
+  })
 
-  function incrementFocus(b){
+  function incrementFocus (b) {
     if (!$('#fuseModal').hasClass('hidden') && window.resultsLength > 0) {
-      if (b == true){
+      if (b == true) {
         if (window.focussedResult < window.resultsLength - 1) {
-          window.focussedResult++ ;
+          window.focussedResult++
         } else {
-          window.focussedResult = 0;
-        };
-      } else if (b == false){
+          window.focussedResult = 0
+        }
+      } else if (b == false) {
         if (window.focussedResult > 0) {
-          window.focussedResult-- ;
+          window.focussedResult--
         } else {
-          window.focussedResult = window.resultsLength - 1 ;
-        };
-      }
-    }
-  };
-
-  function focussResult(n) {
-    if (window.resultsLength > 0){
-      for (var i = 0; i < window.resultsLength; i++) {
-        if ( i == n ) {
-          $("#searchResults > li").eq(i).find("div").attr("id", "selected");
-        } else {
-          $("#searchResults > li").eq(i).find("div").attr("id", "notSelected");
+          window.focussedResult = window.resultsLength - 1
         }
       }
     }
-  };
-
-  // whenever a key is pressed with searchbox in focus, do a search
-  function doSearch(fuse) {
-    let value = document.getElementById("searchBox").value;
-    let results = fuse.search(value);
-    window.resultsLength = results.length;
-
-    let ul = document.getElementById("searchResults");
-    ul.innerHTML = "";
-
-    for (let i = 0; i < results.length; i++) {
-      let item = results[i].item ;
-
-      let li = document.createElement("li");
-
-      let link = document.createElement('a');
-      link.setAttribute('href', item['url']);
-      link.innerHTML = `<div class="w-100">${item["title"]}</div>`;
-
-      li.appendChild(link);
-      ul.appendChild(li);
-    }
-
-    if (window.focussedResult >= 0){
-      focussResult(window.focussedResult);
-    };
   }
 
-  var lazySearch = _.debounce(function(){
-    doSearch(window.fuse);
-  }, 250);
+  function focussResult (n) {
+    if (window.resultsLength > 0) {
+      for (var i = 0; i < window.resultsLength; i++) {
+        if (i == n) {
+          $('#searchResults > li').eq(i).find('div').attr('id', 'selected')
+        } else {
+          $('#searchResults > li').eq(i).find('div').attr('id', 'notSelected')
+        }
+      }
+    }
+  }
 
-  $('#searchBox').on('keydown', function(event) {
+  // whenever a key is pressed with searchbox in focus, do a search
+  function doSearch (fuse) {
+    const value = document.getElementById('searchBox').value
+    const results = fuse.search(value)
+    window.resultsLength = results.length
+
+    const ul = document.getElementById('searchResults')
+    ul.innerHTML = ''
+
+    for (let i = 0; i < results.length; i++) {
+      const item = results[i].item
+
+      const li = document.createElement('li')
+
+      const link = document.createElement('a')
+      link.setAttribute('href', item.url)
+      link.innerHTML = `<div class="w-100">${item.title}</div>`
+
+      li.appendChild(link)
+      ul.appendChild(li)
+    }
+
+    if (window.focussedResult >= 0) {
+      focussResult(window.focussedResult)
+    }
+  }
+
+  var lazySearch = _.debounce(function () {
+    doSearch(window.fuse)
+  }, 250)
+
+  $('#searchBox').on('keydown', function (event) {
     if (event.which == 40) {
-      incrementFocus(true);
-      focussResult(window.focussedResult);
+      incrementFocus(true)
+      focussResult(window.focussedResult)
     } else if (event.which == 74 && event.ctrlKey) {
-      incrementFocus(true);
-      focussResult(window.focussedResult);
+      incrementFocus(true)
+      focussResult(window.focussedResult)
     } else if (event.which == 38) {
-      incrementFocus(false);
-      focussResult(window.focussedResult);
+      incrementFocus(false)
+      focussResult(window.focussedResult)
     } else if (event.which == 75 && event.ctrlKey) {
-      incrementFocus(false);
-      focussResult(window.focussedResult);
+      incrementFocus(false)
+      focussResult(window.focussedResult)
     } else if (event.keyCode == 13) { // return key
-      if (window.focussedResult >= 0){
-        $("#selected").trigger("click");
+      if (window.focussedResult >= 0) {
+        $('#selected').trigger('click')
       } else {
         focussResult(0)
-        $("#selected").trigger("click");
-      };
+        $('#selected').trigger('click')
+      }
     } else if (event.keyCode == 27) { // escape key
-      document.getElementById("searchBox").blur();
-      $('#fuseModal').addClass('hidden');
+      document.getElementById('searchBox').blur()
+      $('#fuseModal').addClass('hidden')
     } else {
-      lazySearch();
+      lazySearch()
     }
-  });
-
+  })
 })
-
